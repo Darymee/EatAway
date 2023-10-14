8aed35608c1af7f1457aa4a3e1f7b1a0b6e62564
bensuerdgn <bensuerdogan00@gmail.com>
2023-10-14 16:11:20 +0200

    adjust design and add cards

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,10 +1,19 @@
 import styles from "./Dashboard.module.css";
-import { FaGlassMartiniAlt, FaHamburger, FaCookieBite } from "react-icons/fa";
+import {
+  FaGlassMartiniAlt,
+  FaHamburger,
+  FaCookieBite,
+  FaRegClock,
+  FaPlus
+} from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 import { getDashboardData } from "./service";
 import { useEffect, useState } from "react";
+import logo from '..//assets/images/logo.png'
 
 export default function Dashboard() {
   const [post, setPost] = useState(null);
+  const [search, setSearch] = useState("");
 
   async function getPosts() {
     const data = await getDashboardData();
@@ -14,9 +23,17 @@ export default function Dashboard() {
     setPost(data);
   }
 
+  const filtered = post?.filter((item) => {
+    return Object.keys(item).some((key) => {
+      return item[key]
+        .toString()
+        .toLowerCase()
+        .includes(search.toLocaleLowerCase());
+    });
+  });
+
   useEffect(() => {
-   getPosts()
-   console.log('hey', post);
+    getPosts();
   }, []);
 
   const filters = [
@@ -33,9 +50,12 @@ export default function Dashboard() {
 
   return (
     <div className="">
-      <div className={styles.header + " pt-4  px-10 pb-10"}>
-        <div className="nav text-start">
-          <p className="">EatAway</p>
+      <div className={styles.header + " pt-4 pb-10 px-10"}>
+        <div className="nav flex justify-between">
+          <img src={logo} alt="logo" className="w-32"/>
+          <button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()}>
+            <FaPlus/>
+          </button>
         </div>
         <div className="flex flex-col justify-center items-center my-20">
           <h1 className="text-white font-black text-5xl uppercase">
@@ -45,35 +65,22 @@ export default function Dashboard() {
             Lorem ipsum sit amet
           </p>
           <div className="form-control">
-            <div className="input-group">
-              <input
-                type="text"
-                placeholder="Search…"
-                className="input input-bordered"
-              />
-              <button className="btn btn-square">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </button>
-            </div>
+            <input
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+              onChange={(e) => setSearch(e.target.value)}
+            />
           </div>
         </div>
 
         <div className="carousel carousel-center max-w-md space-x-4 p-4 bg-white rounded-box">
           {filters.map((el, id) => (
-            <div key={id} className="carousel-item flex flex-col justify-center items-center rounded-box py-4 px-7 gap-2 bg-gray-100">
+            <div
+              key={id}
+              className="carousel-item flex flex-col justify-center items-center rounded-box py-4 px-7 gap-2 bg-gray-100"
+              onClick={() => setSearch(el.name)}
+            >
               {el.icon}
               <p className="rounded-box text-base font-semibold uppercase">
                 {el.name}
@@ -83,23 +90,50 @@ export default function Dashboard() {
         </div>
       </div>
 
-      <div>
-        {post?.length > 0 ? post?.map((el, id) => (
-          <div key={id} className="card w-96 bg-base-100 shadow-xl">
-            <figure>
-              <img src={el.img} alt={el.img} />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {el.title}
-              </h2>
-              <p>{el.description}</p>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">{el.tag}</div>
+      <div className="p-10 gap-4 flex flex-wrap justify-center">
+        {filtered?.length > 0
+          ? filtered?.map((el, id) => (
+              <div key={id} className="card w-96 bg-base-100 shadow-xl">
+                <figure>
+                  <img src={el.img} alt={el.img} />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{el.title}</h2>
+                  <p className="text-start">
+                    {el.city} <br />
+                    <span className="badge badge-outline font-bold text-lg text-amber-700">
+                      {el.price}€
+                    </span>
+                  </p>
+                  <div className="flex justify-between items-end">
+                    <div className="flex justify-center gap-2">
+                      <div className="">
+                        <div className="avatar">
+                          <div className="w-10 rounded-full">
+                            <img src={el.avatarUrl} alt={el.avatarUrl} />
+                          </div>
+                        </div>
+                        <p>{el.username}</p>
+                      </div>
+                      <div className="mt-1">
+                        <span className="flex items-center gap-1 text-xs">
+                          <FaLocationDot className="text-amber-700" />
+                          {el.city}
+                        </span>
+                        <span className="flex items-center gap-1 text-xs">
+                          <FaRegClock className="text-amber-700" /> {el.time}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="card-actions">
+                      <div className="badge badge-outline">{el.location}</div>
+                      <div className="badge badge-outline">{el.tag}</div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        )) : 'yok'}
+            ))
+          : "No results found according to filter"}
       </div>
     </div>
   );
--- src/dashboard/service.js
+++ src/dashboard/service.js
@@ -3,7 +3,6 @@ import axios from "../axios";
 export async function  getDashboardData(){
   try {
     const response = await axios.get("posts");
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
