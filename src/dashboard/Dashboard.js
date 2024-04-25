ea50a2fe2490d01c2e2d0c4e80a02fe46ff04f5b
Darymee <danieledaniele3005@gmail.com>
2024-04-25 17:11:51 +0200

    fixes

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -13,6 +13,7 @@ import { useEffect, useState } from 'react';
 import logo from '..//assets/images/logo.png';
 
 import AddPostModal from '../addPostModal/AddPostModal';
+import { Watch } from 'react-loader-spinner';
 
 export default function Dashboard() {
   const [post, setPost] = useState(null);
@@ -142,51 +143,67 @@ export default function Dashboard() {
         </button>
       )}
 
-      <div className="p-10 gap-8 flex flex-wrap justify-center">
-        {filtered?.length > 0
-          ? filtered?.map((el, id) => (
-              <div key={id} className="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                  <img src={el.img} alt={el.img} className={styles.image} />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{el.title}</h2>
-                  <p className="text-start">
-                    {el.description} <br />
-                    <span className="badge badge-outline font-bold text-lg text-accent my-2">
-                      {el.price}€
-                    </span>
-                  </p>
-                  <div className="flex justify-between items-end">
-                    <div className="flex justify-center gap-2">
-                      <div className="">
-                        <div className="avatar">
-                          <div className="w-10 rounded-full">
-                            <img src={el.avatarUrl} alt={el.avatarUrl} />
+      {post && (
+        <div className="p-10 gap-8 flex flex-wrap justify-center">
+          {filtered?.length > 0
+            ? filtered?.map((el, id) => (
+                <div key={id} className="card w-96 bg-base-100 shadow-xl">
+                  <figure>
+                    <img src={el.img} alt={el.img} className={styles.image} />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{el.title}</h2>
+                    <p className="text-start">
+                      {el.description} <br />
+                      <span className="badge badge-outline font-bold text-lg text-accent my-2">
+                        {el.price}€
+                      </span>
+                    </p>
+                    <div className="flex justify-between items-end">
+                      <div className="flex justify-center gap-2">
+                        <div className="">
+                          <div className="avatar">
+                            <div className="w-10 rounded-full">
+                              <img src={el.avatarUrl} alt={el.avatarUrl} />
+                            </div>
                           </div>
+                          <p>{el.username}</p>
                         </div>
-                        <p>{el.username}</p>
+                        <div className="mt-1">
+                          <span className="flex items-center gap-1 text-xs">
+                            <FaLocationDot className="text-accent" />
+                            {el.city}
+                          </span>
+                          <span className="flex items-center gap-1 text-xs">
+                            <FaRegClock className="text-accent" /> {el.time}
+                          </span>
+                        </div>
                       </div>
-                      <div className="mt-1">
-                        <span className="flex items-center gap-1 text-xs">
-                          <FaLocationDot className="text-accent" />
-                          {el.city}
-                        </span>
-                        <span className="flex items-center gap-1 text-xs">
-                          <FaRegClock className="text-accent" /> {el.time}
-                        </span>
+                      <div className="card-actions">
+                        <div className="badge badge-outline">{el.location}</div>
+                        <div className="badge badge-outline">{el.tag}</div>
                       </div>
-                    </div>
-                    <div className="card-actions">
-                      <div className="badge badge-outline">{el.location}</div>
-                      <div className="badge badge-outline">{el.tag}</div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))
-          : 'No results found according to filter'}
-      </div>
+              ))
+            : 'No results found according to filter'}
+        </div>
+      )}
+
+      {!post && (
+        <div className="flex justify-center">
+          <Watch
+            visible={true}
+            height="80"
+            width="80"
+            radius="48"
+            color="#86A789"
+            ariaLabel="watch-loading"
+          />
+        </div>
+      )}
+
       {isOpen && <AddPostModal onClose={setIsOpen} isOpen={isOpen} />}
     </div>
   );
